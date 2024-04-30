5517102493f29d9bcc497c800768b43aef06ba5f
KateMarmysh <126710416+KateMarmysh@users.noreply.github.com>
2024-04-30 19:30:34 +0500

    Delete agent/run.sh

--- agent/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export `cat config` && python3.7 core/social_agent.py &> agent_errors.logs &
-python3.7 -m http.server 80 --directory web &> http_errors.logs &
-echo 'starting services..'
-sleep 5
-echo 'put data..'
-python3.7 core/put_data.py

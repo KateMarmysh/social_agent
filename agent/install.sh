ae2a8c9f91796e6d2cd47483fbd09e9b1d4777c0
KateMarmysh <126710416+KateMarmysh@users.noreply.github.com>
2024-04-30 19:31:40 +0500

    upload file install.sh

---
+++ agent/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install setuptools==59.6.0
+pip install owlready2==0.45
+pip install vk_api==11.9.9
+pip install pika==1.3.2
+pip install networkx==3.2.1
+pip install requests==2.31.0
